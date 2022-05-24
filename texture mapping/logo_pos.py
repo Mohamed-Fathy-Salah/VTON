53deef8a9461b79145e0f64c0538759bc144e3e8
Mohamed Fathy <mofasa.peace@gmail.com>
2022-05-24 14:18:32 +0200

    draw rect around tshirt, logo (kinda sorta works)

---
+++ texture mapping/logo_pos.py
@@ -0,0 +1,31 @@
+from turtle import hideturtle
+import cv2 as cv 
+import numpy as np
+import matplotlib.pyplot as plt
+
+def pos(mask, img):
+    # cnts = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    # cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    # mx = 0
+    # shirt pos
+    # xm, ym, wm, hm = 0,0,0,0
+    # for c in cnts:
+    #     x,y,w,h = cv.boundingRect(c)
+    #     if mx < w * h:
+    #         mx = w*h
+    #         xm, ym, wm, hm = x,y,w,h 
+
+    xm, ym, wm, hm = cv.boundingRect(cv.findNonZero(mask))
+    cv.rectangle(img, (xm, ym), (xm + wm, ym + hm), (0,0,255), 2)
+
+    cv.imshow('image', img)
+    cv.waitKey()
+    return xm, ym, wm, hm
+
+mask = cv.imread("./images/mask.png", cv.IMREAD_GRAYSCALE)
+img = cv.imread("./images/dubai.jpg")
+logo = cv.imread("results/light.png", cv.IMREAD_GRAYSCALE)
+
+
+pos(mask, img)
+pos(logo, img)

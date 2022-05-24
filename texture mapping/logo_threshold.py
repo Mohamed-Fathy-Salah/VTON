95b1bb9ca70d4eb13264ff7c7c28e10906e83930
Mohamed Fathy <mofasa.peace@gmail.com>
2022-05-24 14:17:51 +0200

    fixed paths

--- texture mapping/logo_threshold.py
+++ texture mapping/logo_threshold.py
@@ -8,8 +8,8 @@ def equal(a, b):
     # print(val)
     return val < 20000
 
-img_path = "./images/dubai.jpg"
-mask_path = "images/mask.png"
+img_path = "./images/shirt.jpg"
+mask_path = "images/shirt_mask.png"
 
 
 in_img = cv2.imread(img_path)
@@ -28,4 +28,4 @@ out_img = cv2.bitwise_and(out_img, out_img, mask=mask_img)
 out_img = cv2.medianBlur(out_img, 3)
 
 show("out", out_img)
-cv2.imwrite("./results/dubai.png", out_img)
+cv2.imwrite("./results/shirt_logo.png", out_img)

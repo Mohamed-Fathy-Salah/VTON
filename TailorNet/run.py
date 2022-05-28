ee57180addf7f52abc7515a4738cddadc6f426c4
Mohamed Fathy <mofasa.peace@gmail.com>
2022-05-28 11:41:12 +0200

    -

--- TailorNet/run.py
+++ TailorNet/run.py
@@ -1,16 +1,20 @@
 import numpy as np
 import torch
 
+from visualization.vis_utils import get_specific_pose
+from visualization.vis_utils import get_specific_shape
+
 from models.tailornet_model import get_best_runner as get_tn_runner
 from utils.rotation import normalize_y_rotation
 
+from models.smpl4garment import SMPL4Garment
 # from dataset.canonical_pose_dataset import get_style, get_shape
 from dataset.canonical_pose_dataset import get_style
 from utils.interpenetration import remove_interpenetration_fast
 
 # Set output path where inference results will be stored
 OUT_PATH = "/content/output"
-def gen_gar(theta,beta,gender,garment_class,filename):
+def generate_garment(theta=get_specific_pose(0), beta=get_specific_shape('mean'), gender='female', garment_class='t-shirt', filename='out'):
     gamma = get_style('000',garment_class=garment_class,gender=gender)
     tn_runner = get_tn_runner(gender=gender, garment_class=garment_class)
     theta_normalized = normalize_y_rotation(theta)
@@ -20,7 +24,12 @@ def gen_gar(theta,beta,gender,garment_class,filename):
             betas=torch.from_numpy(beta[None, :].astype(np.float32)),#.cuda(),
             gammas=torch.from_numpy(gamma[None, :].astype(np.float32)),#.cuda(),
         )[0].cpu().numpy()
+
+    smpl = SMPL4Garment(gender=gender)
     body, pred_gar = smpl.run(beta=beta, theta=theta, garment_class=garment_class, garment_d=pred_verts_d)
     pred_gar = remove_interpenetration_fast(pred_gar, body)
+
     pred_gar.write_ply("./src/models/ply/"+filename+".ply")
     pred_gar.write_obj("./src/models/obj/"+filename+".obj")
+
+generate_garment()

e9a0b72b232a0ad3c9ee1577fcb6ebd930e3d5b6
Mohamed Fathy <mofasa.peace@gmail.com>
2022-06-13 14:51:53 +0200

    split top, bottom

--- TailorNet/run.py
+++ TailorNet/run.py
@@ -11,13 +11,14 @@ from models.smpl4garment import SMPL4Garment
 from dataset.canonical_pose_dataset import get_style
 from utils.interpenetration import remove_interpenetration_fast
 
-OUT_PATH = "/content/output"
+OUT_PATH = "../../output"
+TXT_PATH = "../../dataset/txt/"
 
 def write_obj(mesh, garment=None, gender=None, filename=None):
     if garment:
-        coord_filename = f"{garment}_{gender}.txt"
+        coord_filename = f"{TXT_PATH}{garment}_{gender}.txt"
     else:
-        coord_filename = "body_tex_coords.txt"
+        coord_filename = f"{TXT_PATH}body_tex_coords.txt"
 
     with open(filename, 'w') as output:
         for r in mesh.v: 
@@ -26,7 +27,7 @@ def write_obj(mesh, garment=None, gender=None, filename=None):
         with open(coord_filename) as f:
             output.write(f.read())
 
-def generate_body(theta=get_specific_pose(0), beta=get_specific_shape('mean'), gender='female', filename='body'):
+def generate_body(theta=get_specific_pose(0), beta=get_specific_shape('mean'), gender='female'):
     smpl = SMPL4Garment(gender=gender)
     body,_ = smpl.run(beta=beta, theta=theta)
     return body
@@ -49,19 +50,31 @@ def generate_body_garment(theta=get_specific_pose(0), beta=get_specific_shape('m
 
     return body, pred_gar
 
-# size [top, bottom]
-# garment_class [top, bottom]
-def run(theta, beta, gender, sizes, garments, save_body):
-    assert len(sizes) != len(garments) and sizes
+#top_garment = [garment type, garment size]
+def run(theta, beta, gender, top_garment, bottom_garment, save_body):
+    body_bottom_gar_combined = None
+    body = None
 
-    body, top_gar = generate_body_garment(theta=theta, beta=beta, gender=gender, garment_class=garments[0], size=sizes[0], save_body=True)
-    top_gar = remove_interpenetration_fast(top_gar, body)
-    write_obj(top_gar, garment=garments[0], gender=gender, filename=f"{OUT_PATH}/{garments[0]}.obj")
+    if bottom_garment:
+        garment, size = bottom_garment[:]
 
-    if len(sizes) > 1:
-        _, bottom_gar = generate_body_garment(theta=theta, beta=beta, gender=gender, garment_class=garments[1], size=sizes[1], save_body=False)
-        bottom_gar = remove_interpenetration_fast(bottom_gar, body)
-        write_obj(bottom_gar, garment=garments[1], gender=gender, filename=f"{OUT_PATH}/{garments[1]}.obj")
+        body, bottom_gar = generate_body_garment(theta=theta, beta=beta, gender=gender, garment_class=garment, size=size, save_body=True)
+        top_gar = remove_interpenetration_fast(bottom_gar, body)
+
+        body_bottom_gar_combined = bottom_gar.concatenate_mesh(body)
+
+        write_obj(top_gar, garment=garment, gender=gender, filename=f"{OUT_PATH}/{garment}.obj")
+
+    if top_garment:
+        garment, size = top_garment[:]
+
+        body, top_gar = generate_body_garment(theta=theta, beta=beta, gender=gender, garment_class=garment, size=size, save_body=True)
+        
+        if body_bottom_gar_combined:
+            top_gar = remove_interpenetration_fast(top_gar, body_bottom_gar_combined)
+        else:
+            top_gar = remove_interpenetration_fast(top_gar, body)
+        write_obj(top_gar, garment=garment, gender=gender, filename=f"{OUT_PATH}/{garment}.obj")
 
     if save_body :
         write_obj(body, filename=f"{OUT_PATH}/body.obj")
@@ -69,6 +82,8 @@ def run(theta, beta, gender, sizes, garments, save_body):
 if __name__ == '__main__':
     run(get_specific_pose(0), get_specific_shape('mean'), 'female', ['000', '000'], ['t-shirt', 'short-pant'], True)
 
-    # body, gar = generate_body_garment(save_body=True)
-    # write_obj(gar, garment="short-pant", gender="female", filename=f"{OUT_PATH}/short-pant.obj")
+    # body, gar = generate_body_garment(garment_class='pant', gender='male', save_body=True)
+    # body.write_obj(f"{OUT_PATH}/pant.obj")
+    # gar.write_obj(f"{OUT_PATH}/pant.obj")
+    # write_obj(gar, garment="pant", gender="male", filename=f"{OUT_PATH}/pant.obj")
     # write_obj(body, filename=f"{OUT_PATH}/female.obj")
